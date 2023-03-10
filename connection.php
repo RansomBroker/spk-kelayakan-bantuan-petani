8fd8980df8b78699c699b9ae47bd10c86447cf8a
RansomBroker <yadisoke@gmail.com>
2023-03-10 17:41:07 +0700

    fungsi upate data petani

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-$connection = new mysqli ("lnpp-8-mysql-db", "root", "root", "db_petani");
-
-if ($connection->connect_error != null) {
-    echo  "Gagal terhubung ke Database";
-    die();
-}
-
