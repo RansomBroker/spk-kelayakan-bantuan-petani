17a3bf35d18b21ee8a86367ef3eb68e84f86b1c1
Azdirazi <91419014+Azdirazi@users.noreply.github.com>
2023-03-15 12:04:05 +0700

    setting

--- settings.php
+++ settings.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+    include "function.php";
+    
+    if (isset($_POST['setinggs'])) {
+        setting($_POST);
+    }
+
+    $data_setinggs = savesetting();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,10 +31,57 @@
 
             <!-- Main Content -->
             <div id="content">
-            <?php include "navbar.php";?>
+                <?php include "navbar.php";?>
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
-        </div>
+                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                    <h1 class="h3 mb-0 text-gray-800">Setting Kriteria </h1>
+                </div>
+
+                <div class="card card-body">
+                    <?php if (get_flash_name('failed_settings') != ""):?>
+                        <div class="alert alert-danger">
+                            <?= get_flash_message('failed_settings')?>
+                        </div>
+                    <?php endif;?>
+                    <form method="POST" id>
+                        <div class="card card-body">
+                            <h5 class="card-title">Atur Nilai Kriteria</h5>
+                            <div class="row">
+                                <input type="hidden" name="id" value="">
+                                <div class="col-lg-3 col-12 mb-2">
+                                    <label class="form-label">Luas Lahan  <sup class="text-danger">*</sup></label>
+                                    <input type="text" class="form-control desimal-input" name="luas_lahan" value='<?= $data_setinggs['luas_lahan']?>' required>
+                                </div>
+                                <div class="col-lg-4 col-12 mb-2">
+                                    <label class="form-label">Penghasilan <sup class="text-danger">*</sup></label>
+                                    <input type="text" class="form-control desimal-input" name="penghasilan" value='<?= $data_setinggs['penghasilan']?>' required>
+                                </div>
+                                <div class="col-lg-4 col-12 mb-2">
+                                    <label class="form-label">Hasil Panen<sup class="text-danger">*</sup></label>
+                                    <input type="text" class="form-control desimal-input" name="hasil_panen" value='<?= $data_setinggs['hasil_panen']?>' required>
+                                </div>
+                                <div class="col-lg-3 col-12 mb-2">
+                                    <label class="form-label">Lama Usaha Tani <sup class="text-danger">*</sup></label>
+                                    <input type="text" class="form-control desimal-input" name="lama_usaha_tani" value='<?= $data_setinggs['lama_usaha_tani']?>' required>
+                                </div>
+                                <div class="col-lg-4 col-12 mb-2">
+                                    <label class="form-label">Jumlah Anggota Keluarga <sup class="text-danger">*</sup></label>
+                                    <input type="text" class="form-control desimal-input" name="jmlh_anggota_keluarga" value='<?= $data_setinggs['jmlh_anggota_keluarga']?>' required>
+                                </div>
+                            </div>
+                            <div class="col-12 p-0 mt-2">
+                                <div class="d-flex justify-content-start">
+                                    <button type="submit" name="setinggs" class="btn btn-primary mr-2">Simpan</button>
+                                    <button type="reset"  class="btn btn-danger mx-2">Batal</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+                
+            </div>
         <!-- End of Content Wrapper -->
 
     </div>
@@ -56,5 +113,16 @@
     </div>
 
     <?php include "js.php"?>
+    <script>
+       $(document).on('keyup', '.desimal-input', function(e) { 
+
+        var regex = /[^\d.]|\.(?=.*\.)/;
+        var subst = "";
+        var str    = $(this).val();
+        var result = str.replace(regex, subst);
+        $(this).val(result);
+
+            });
+    </script>
 </body>
 </html>
