1221f5af805baa7ca1e775546ce83fec3bbde7e2
RansomBroker <yadisoke@gmail.com>
2023-03-16 14:29:41 +0700

    asdfasdf

---
+++ connection.example.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+$connection = new mysqli ("localhost", "root", "", "db_petani");
+
+if ($connection->connect_error != null) {
+    echo  "Gagal terhubung ke Database";
+    die();
+}
