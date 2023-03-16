b1c79bdb133823bec625c82bdd719713b7b1ce7d
RansomBroker <yadisoke@gmail.com>
2023-03-16 14:33:24 +0700

    asdfsd

--- hapus-data-alternatif.php
+++ hapus-data-alternatif.php
@@ -5,7 +5,7 @@ include "helper.php";
 
 $id = $_GET['id'];
 $query = $connection->query("
-    DELETE FROM data_alternatif WHERE id = '$id'
+    DELETE FROM data_kriteria WHERE id = '$id'
 ");
 
 redirect('olah-data.php?halaman=olah-data');
