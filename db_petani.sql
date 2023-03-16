d5067ea4dd5734a767024b1c7bfa63af75616bc3
Azdirazi <91419014+Azdirazi@users.noreply.github.com>
2023-03-16 14:10:25 +0700

    barubro

--- db_petani.sql
+++ db_petani.sql
@@ -1,17 +1,14 @@
 /*
  Navicat Premium Data Transfer
-
  Source Server         : lnpp-8-mysql
  Source Server Type    : MySQL
  Source Server Version : 50722
  Source Host           : 17.17.17.5:3306
  Source Schema         : db_petani
-
  Target Server Type    : MySQL
  Target Server Version : 50722
  File Encoding         : 65001
-
- Date: 09/03/2023 14:55:56
+ Date: 16/03/2023 13:58:30
 */
 
 SET NAMES utf8mb4;
@@ -24,15 +21,15 @@ DROP TABLE IF EXISTS `data_kriteria`;
 CREATE TABLE `data_kriteria` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
   `id_petani` int(255) NOT NULL,
-  `nama_petani` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `luas_lahan` int(255) NOT NULL,
   `penghasilan` int(255) NOT NULL,
+  `hasil_panen` int(255) NOT NULL,
   `lama_usaha_tani` int(255) NOT NULL,
   `jmlh_anggota_keluarga` int(255) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   KEY `data_kriteria_id_petani_foreign` (`id_petani`),
   CONSTRAINT `data_kriteria_id_petani_foreign` FOREIGN KEY (`id_petani`) REFERENCES `data_petani` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Table structure for data_petani
@@ -47,21 +44,21 @@ CREATE TABLE `data_petani` (
   `no_ktp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tgl_pemohonan` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Table structure for hasil
 -- ----------------------------
 DROP TABLE IF EXISTS `hasil`;
 CREATE TABLE `hasil` (
-  `id` int(255) NOT NULL,
+  `id` int(255) NOT NULL AUTO_INCREMENT,
   `id_petani` int(255) NOT NULL,
-  `nilai_akhir` decimal(6,0) NOT NULL,
+  `nilai_akhir` decimal(65,4) NOT NULL,
   `status` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `hasil_user_id_foreign` (`id_petani`),
   CONSTRAINT `hasil_user_id_foreign` FOREIGN KEY (`id_petani`) REFERENCES `data_petani` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=617 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Table structure for settings
@@ -69,13 +66,14 @@ CREATE TABLE `hasil` (
 DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` (
   `id` int(255) NOT NULL AUTO_INCREMENT,
-  `luas_lahan` decimal(65,2) NOT NULL,
-  `penghasilan` decimal(65,2) NOT NULL,
-  `hasil_panen` decimal(65,2) NOT NULL,
-  `lama_usaha_tani` decimal(65,2) NOT NULL,
-  `jmlh_anggota_keluarga` decimal(65,2) NOT NULL,
+  `luas_lahan` decimal(65,4) NOT NULL,
+  `penghasilan` decimal(65,4) NOT NULL,
+  `hasil_panen` decimal(65,4) NOT NULL,
+  `lama_usaha_tani` decimal(65,4) NOT NULL,
+  `jmlh_anggota_keluarga` decimal(65,4) NOT NULL,
+  `v` decimal(65,4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- ----------------------------
 -- Table structure for users
@@ -86,6 +84,6 @@ CREATE TABLE `users` (
   `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1
